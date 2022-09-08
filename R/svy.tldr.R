bbf51b578b243aa3d3c98f066fc5f01ed5bf9164
AJThurston <35585810+AJThurston@users.noreply.github.com>
2022-09-08 14:42:19 -0400

    renamed variables with underscores

--- R/svy.tldr.R
+++ R/svy.tldr.R
@@ -1,6 +1,6 @@
 # JAMRS SURVEY ANALYSIS CODE
 # Thurston, AJ
-# 2020-09-08
+# 2020-01-14
 #
 # Analysis notes:
 # _f = factors for the survey library for weighted estimates
@@ -11,6 +11,8 @@
 # 2020-01-07 Start
 # 2020-01-14 Combining M,SE,N results into one table
 
+
+
 #' svy.tldr
 #'
 #' @param df A survey dataframe consisting of at minimum a survey item formatted as a factor variable.
@@ -19,17 +21,17 @@
 #' @param weights Survey weights (optional)
 #' @param svyitem A survey item with factor (or ordered factor) format
 #' @param svygrp A survey grouping variable, can be binary or multiple group, in factor format (optional)
-#' @param fltr.refuse Filter refusals formatted 'refused' (Default = TRUE)
-#' @param fltr.nas Filter NAs across dataframe (Default = TRUE)
-#' @param low.n.flg Flag estimates with less than n = 100 in either svyitem response option or svygroup (or the combination thereof)
-#' @param fmttd.tbl Produces a formatted table with columns for each group and statistic (Default = F; statistics nested w/in group)
+#' @param fltr_refuse Filter refusals formatted 'refused' (Default = TRUE)
+#' @param fltr_nas Filter NAs across dataframe (Default = TRUE)
+#' @param flg_low_n Flag estimates with less than n = 100 in either svyitem response option or svygroup (or the combination thereof)
+#' @param fmttd_tbl Produces a formatted table with columns for each group and statistic (Default = F; statistics nested w/in group)
 #'
 #' @return A tibble with M, SE, and unweighted Ns for each response for svyitem (or each response for svyitem within svygroup)
 #' @export
 #'
 #' @examples
-#' svy.tldr(df = df, ids = "ids", strata = "strata", weights = "wt", svyitem = "svyitem", svygrp = "group")
-svy.tldr <- function(df,ids,strata,weights,svyitem,svygrp,fltr.refuse=T,fltr.nas=T,low.n.flg = F, fmttd.tbl = F){
+#' svy.tldr(df = df, ids = id, strata = strata, weights = wt, svyitem = "svyitem", svygrp = "group")
+svy.tldr <- function(df,ids,strata,weights,svyitem,svygrp,fltr_refuse=T,fltr_nas=T,flg_low_n = F, fmttd_tbl = F){
   
   options(survey.lonely.psu="adjust")
   
@@ -59,28 +61,31 @@ svy.tldr <- function(df,ids,strata,weights,svyitem,svygrp,fltr.refuse=T,fltr.nas
     
   }
   
-  if(low.n.flg == T){
-    res$low.n.flg <- ifelse(res$n >= 100, 0, 1)
+  if(flg_low_n == T){
+    res$low_n_flg <- ifelse(res$n >= 100, 0, 1)
   }
   
-  if(fltr.refuse == T){
+  if(fltr_refuse == T){
     res <- res[!res[1] == "Refused",] # sometimes capitalized
     res <- res[!res[1] == "refused",] # somtimes not capitalized    
     res <- res[!res[2] == "Refused",] # sometimes capitalized
     res <- res[!res[2] == "refused",] # somtimes not capitalized
   }
   
-  if(fltr.nas == T){
+  if(fltr_nas == T){
     res <- res[complete.cases(res), ]
   }
   
-  if(fmttd.tbl == T){
+  if(fmttd_tbl == T){
+    
+    values <- names(res[,3:ncol(res)])
+    
     res <- res %>%
       group_by(group, res[,svyitem]) %>%
       pivot_wider(svyitem, 
                   names_from = "group", 
                   names_glue = "{group}.{.value}",
-                  values_from = c("m","m_se","n"),
+                  values_from = all_of(values),
                   names_vary = 'slowest')
     res
   
